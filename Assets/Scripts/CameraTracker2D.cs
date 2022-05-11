51fb34d97ffd348f1390ddc2974681bd6ab609ff
Excel Ortega <excel.ortega@gmail.com>
2022-05-11 16:39:27 -0700

    adding smooth follow and room size camera trap

--- Assets/Scripts/CameraTracker2D.cs
+++ Assets/Scripts/CameraTracker2D.cs
@@ -1,23 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class CameraTracker2D : MonoBehaviour
 {
     [SerializeField]
     protected bool isCameraTrap;
 
+    /// <summary>
+    /// Change the size of the camera trap to be the size of the camera viewport.
+    /// </summary>
+    [SerializeField]
+    protected bool isSizeOfViewPort;
+
     [SerializeField]
     protected Vector2 cameraTrapSize = new Vector2(3, 3);
-
-    private Bounds cameraTrapBounds;
 
     [SerializeField]
     protected Transform trackableObject;
 
+    /// <summary>
+    /// Approximately the time it will take the camera to reach the target in seconds.
+    /// The smaller the value, the faster the camera will reach the target.
+    /// </summary>
+    [SerializeField]
+    protected float followSpeed = 0.125f;
+
+    private Bounds cameraTrapBounds;
+
+    private Camera cam;
+    
+    /// <summary>
+    /// Used to keep track of how fast the camera follow is moving at a particular frame
+    /// It is also needed by the implemention of Unity's Vector3.SmoothDamp
+    /// </summary>
+    private Vector3 velocity = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
+        cam = GetComponent<Camera>();
+        if (isSizeOfViewPort)
+        {
+            cameraTrapSize = new Vector2(cam.orthographicSize * cam.aspect, cam.orthographicSize) * 2;
+        }
         cameraTrapBounds = new Bounds(trackableObject.position, cameraTrapSize);
         transform.position = new Vector3(cameraTrapBounds.center.x, cameraTrapBounds.center.y, transform.position.z);
     }
@@ -53,8 +78,12 @@ public class CameraTracker2D : MonoBehaviour
         {
             cameraTrapBounds.center = trackableObject.position;
         }
+    }
 
-        transform.position = new Vector3(cameraTrapBounds.center.x, cameraTrapBounds.center.y, transform.position.z);
+    private void LateUpdate()
+    {
+        Vector3 targetPosition = new Vector3(cameraTrapBounds.center.x, cameraTrapBounds.center.y, transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, followSpeed);
     }
 
     private void OnDrawGizmos()
