e84a635284f8dab3bfb8ee0a23a96f6871334f34
Excel Ortega <excel.ortega@gmail.com>
2022-07-13 17:54:32 -0700

    Adding new game tags to support newly defined inspector tags.

--- Assets/Scripts/GameTag.cs
+++ Assets/Scripts/GameTag.cs
@@ -3,6 +3,8 @@
 /// </summary>
 public static class GameTag
 {
+    public static readonly string Player = "Player";
     public static readonly string Enemy = "Enemy";
     public static readonly string Projectile = "Projectile";
+    public static readonly string Consumable = "Consumable";
 }
