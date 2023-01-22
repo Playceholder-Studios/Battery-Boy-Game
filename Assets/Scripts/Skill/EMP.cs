8132ba986be3679f1b6348b2611d25e6101a6171
Excoh <excel.ortega@gmail.com>
2023-01-21 22:06:21 -0800

    Fixing EMP Bug

--- Assets/Scripts/Skill/EMP.cs
+++ Assets/Scripts/Skill/EMP.cs
@@ -1,22 +1,23 @@
 using UnityEngine;
 
-[RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(Collider2D))]
 public class EMP : Skill
 {
-    public float strength = 1.0f;
+    public float pushStrength = 1.0f;
     public GameObject empParticleSystem;
     private ParticleSystem ps;
+    private Collider2D col;
 
     public override void Activate()
     {
-        gameObject.SetActive(true);
+        col.enabled = true;
         ps?.Play();
     }
 
     private void Awake()
     {
-        gameObject.SetActive(false);
         ps = empParticleSystem.GetComponent<ParticleSystem>();
+        col = GetComponent<Collider2D>();
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -25,9 +26,8 @@ public class EMP : Skill
         {
             var enemy = other.GetComponent<Enemy>();
             var dirToMove = Vector3.Normalize(other.transform.position - transform.position);
-            enemy.MoveToTarget(other.transform.position + (dirToMove * strength), 10.0f, true);
-            gameObject.SetActive(false);
+            enemy.MoveToTarget(other.transform.position + (dirToMove * pushStrength), 10.0f, true);
+            col.enabled = false;
         }
-
     }
 }
